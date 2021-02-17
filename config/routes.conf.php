7c41cf93f514c88d32a8c4c057527a7b294fca2b
andrey <andrey.artemiev@intermobility.ru>
2021-02-17 09:50:46 +0700

    add rout

--- config/routes.conf.php
+++ config/routes.conf.php
@@ -16,7 +16,7 @@ return [
         "/^\/{0,1}catalog\/(?P<alias>.{1,})$/i" => ["FrontEnd", "Page", "Index", ['alias' => 'catalog_page', "catalog_alias" => '$$alias']],
         "/^\/{0,1}product\/(?P<alias>.{1,})$/i" => ["FrontEnd", "Page", "Index", ['alias' => 'product_page', "product_alias" => '$$alias']],
         "/^\/{0,1}search$/i" => ["FrontEnd", "Search", "Index", []],
-        "/^\/{0,1}Soap\/(?P<alias>\d{1,})(?P<translit_name>.*)$/i" => ["FrontEnd", "Page", "Index", ['alias' => 'soap_page', "soap_id" => '$$alias',"translit_name" => '$$translit_name']],
+        "/^\/{0,1}soap\/(?P<alias>\d{1,})(?P<translit_name>.*)$/i" => ["FrontEnd", "Page", "Index", ['alias' => 'soap_page', "soap_id" => '$$alias',"translit_name" => '$$translit_name']],
         "/^\/{0,1}profile$/i" => ["FrontEnd", "Cabinet", "Profile", ['r'=>0]],
         "/^\/{0,1}News\/(?P<alias>\d{1,})$/i" => ["FrontEnd", "Page", "Index", ['alias' => 'news_page', "content_id" => '$$alias']],
         "/^\/{0,1}NewsList\/(?P<page>\d{1,})$/i" => ["FrontEnd", "NewsList", "Index", ["page" => '$$page']],
